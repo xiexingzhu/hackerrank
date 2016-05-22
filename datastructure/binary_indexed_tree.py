10421d15ccf1890ad874e47bdc0cbfaec5f26cc3
xiexingzhu <xiexingzhu1992330@gmail.com>
2016-05-22 19:55:14 +0800

    python binary indexed tree

---
+++ datastructure/binary_indexed_tree.py
@@ -0,0 +1,30 @@
+#here is a python code to implement 
+#binary indexed tree
+#which is very useful data structure
+#in certain circumstances
+def get(BITree,i):
+    s=0 #initialize result
+    
+    i=i+1#index in BITree is 1 more than the index in arr[]
+    
+    while i>0:
+        s+= BITree[i]# add current element of BITree to sum
+        i-=i & (-i)
+    return s
+    
+#updated a node in BITree
+def updatebit(BITree,n,i,v):
+    
+    i+=1
+    
+    while i <= n:
+        BITree[i]+=v
+        i+= i & (-i)
+        
+def construct(arr,n):
+    BITree=[0]*(n+1) # Create and initialize BITree as 0
+    
+    for i in range(n):
+        updatebit(BITree,n,i,arr[i])
+        
+    return BITree
