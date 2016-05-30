2691dd41d406e97ca41aa0b08d2199cbe200d5f1
xiexingzhu <xiexingzhu1992330@gmail.com>
2016-05-30 19:20:35 +0800

    totally have no idea

---
+++ datastructure/Simple Game.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 30 13:52:57 2016
+
+@author: kid
+
+hackerrank  week-20 Chanllenges Simple Game
+"""
+
+N,M,K =map(int,raw_input().strip().split())
+
+tmp=[1]*N
+
+count=0
+
+
