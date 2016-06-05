bb336a3004a5297ff9ea9b1908bf579a69522681
xiexingzhu <xiexingzhu1992330@gmail.com>
2016-06-05 23:02:59 +0800

    Done, briliant

--- datastructure/equal.py
+++ datastructure/equal.py
@@ -1,36 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jun 03 16:01:31 2016
+Created on Sun Jun 05 22:19:05 2016
 
 @author: kid
-
-hackerrank Algorithms Dynamic Programming  Equal
 """
-
 l={0:0,1:1,2:1,3:2,4:2,5:1}
 
-def equal(a,m,n):
-    if n-m==1:
-        if a[m]==a[n]:
-            return 0
-        else:
-            count =a[n]-a[m]
-            Count=count/5
-            count=count%5
-            
-            return l[count]+Count
+def findway(a,b):
+    count=0
+    for i in range(len(a)):
+        d=a[i]-b
+        if d>5:
+            count+=d/5
+            d=d%5
         
+        count+=l[d]
+    return count
         
-    else:
-        count=a[n]-a[m]
-        tmp=equal(a,m,n-1)
-        Count=count/5
-        count=count%5
-        tmp+=Count
-        #print count
-        tmp+=l[count]
-            
-        return tmp
-            
-            
-            
+def equal(a):
+    m=min(a)
+    
+    return min(findway(a,m),findway(a,m-1),findway(a,m-2),findway(a,m-3),findway(a,m-4))
