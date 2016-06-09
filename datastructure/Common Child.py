4033600484596cb429ee7005db555c49536dedc6
xiexingzhu <xiexingzhu1992330@gmail.com>
2016-06-09 14:33:54 +0800

    i think this one is good enough , but it still fails the last five cases in hackerrank, maybe there exists more efficient algorithm

--- datastructure/Common Child.py
+++ datastructure/Common Child.py
@@ -7,17 +7,21 @@ Created on Thu Jun 09 13:26:12 2016
 hackerrank Algorithms Strings Common Child
 """
 
-def commonchild(a,b,a1,b1):
-    if a1==0 or b1==0:
-        if len(set(a[:a1+1]).intersection(set(b[:b1+1])))==1:
-            return 1
-        else:
-            return 0
-    else:
-        if a[a1]==b[b1]:
-            return commonchild(a,b,a1-1,b1-1)+1
-        else:
-            return max(commonchild(a,b,a1-1,b1),commonchild(a,b,a1,b1-1))
+def commonchild(a,b):
+    m=len(a)
+    n=len(b)
+    
+    mat=[[0]*5001 for i in range(5001)]
+    
+    for i in range(m):
+        for j in range(n):
+            if a[i]==b[j]:
+                mat[i+1][j+1]=mat[i][j]+1
+            else:
+                mat[i+1][j+1]=max(mat[i][j+1],mat[i+1][j])
+                
+    return mat[m][n] 
+    
             
             
         
