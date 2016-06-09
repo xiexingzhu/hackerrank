6620b7465f6ea2e3941193f136810f3814d12f8e
xiexingzhu <xiexingzhu1992330@gmail.com>
2016-06-09 15:23:22 +0800

    Done

---
+++ datastructure/Sherlock and Valid String.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 09 14:50:17 2016
+
+@author: kid
+
+hackerrank Algorithms Strings Sherlock and Valid String
+"""
+from collections import Counter
+def sherlock(a):
+    a=Counter(a)
+    b=Counter(a.values()).most_common(1)[0][0]    
+    
+    count=0
+    for i in a.values():
+        if i==1 and b-i>1:
+            count+=1
+        elif i-b==1:
+            count+=1
+        else:
+            count+=abs(i-b)
+        
+
+    
+    print 'YES' if count<2 else 'NO'
+    
+    
