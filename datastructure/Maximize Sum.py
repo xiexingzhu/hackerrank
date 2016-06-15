ccf0a5c31f173350c3a08ad259232eeb266d9373
xiexingzhu <xiexingzhu1992330@gmail.com>
2016-06-15 14:43:50 +0800

    brilliant algorithm , think about why we should only calculate a[i] and a[i-1] during the process

--- datastructure/Maximize Sum.py
+++ datastructure/Maximize Sum.py
@@ -7,19 +7,20 @@ Created on Tue Jun 14 13:47:27 2016
 hackerrank Algorithms Search Maximize Sum
 """
 
-def maximizesum(a,m):
-    a[0]=a[0]%m
-    for i in range(1,len(a)):
-        a[i]=(a[i]+a[i-1])%m
-        
-    count=0
-    for i in range(1,len(a)):
-        for j in range(i-1,-1,-1):
-            if a[j]>a[i]:
-                count=max(count,((a[i]-a[j])+m)%m)
-        count=max(count,a[i])
-        
-    print count
+T = int(raw_input())
+for t in range(T):
+    [N, M] = [int(x) for x in raw_input().split()]
+    array = [int(x) for x in raw_input().split()]
+    prefix_sum = [array[0] % M]
+    for i in range(1, N):
+        prefix_sum.append((prefix_sum[i - 1] + array[i]) % M)
+    bi_sum = [(prefix_sum[i], i) for i in range(N)]
+    bi_sum.sort()
+    mmod = bi_sum[-1][0]
+    for i in range(1, N):
+        if bi_sum[i][1] < bi_sum[i-1][1] and M - (bi_sum[i][0] - bi_sum[i-1][0]) > mmod:
+            mmod = M - (bi_sum[i][0] - bi_sum[i-1][0])
+    print mmod
                 
                 
                 
